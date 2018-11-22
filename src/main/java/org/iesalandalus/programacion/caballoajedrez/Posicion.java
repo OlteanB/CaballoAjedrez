de2b4a311245e9fb54c6c2da6b60ad50f1c72f74
Bogdan <Bogdan@DESKTOP-1BA6BTE>
2018-11-22 22:24:04 +0100

    Commit despues de acabar con la clase Posición y un monton de corecciónes

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -3,83 +3,99 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
-	
-	
-	 /**
-     * Constructor por defecto
-     */
-    public Posicion() {
-        fila = 1;
-        columna = 'a';
-    }
-    
-    /**
-     * Costructor que recibe dos parametros y controla que los valores introducidos son correctos
-     * @param fila
-     * @param columna 
-     * @throws IllegalAccessException 
-     */
-    public Posicion(int fila, char columna) throws IllegalAccessException {
-        this.fila = fila;
-        this.columna = columna;
-        if (fila > 0 && fila < 9) {
-            this.fila = fila;
-        } else {
-            this.fila = 1;
-            throw new IllegalAccessException("ERROR: Fila no valida.");
-        }
-        if (columna >= 'a' && columna <= 'h') {
-            this.columna = columna;
-        } else {
-            this.columna = 'a';
-            throw new IllegalAccessException("ERROR: Columna no valida.");
-        }
-    }
-	
+
 	/**
-     * 
-     * @return fila
-     */
-    public int getFila() {
-        return fila;
-    }
+	 * Constructor por defecto
+	 */
+	public Posicion() {
+		fila = 1;
+		columna = 'a';
+	}
 
-    /**
-     * Setter que recibe y controla los errores de la fila
-     * @param fila 
-     * @throws IllegalAccessException 
-     */
-    public void setFila(int fila) throws IllegalAccessException {
-        this.fila = fila;
-        if (fila > 0 && fila < 9) {
-            this.fila = fila;
-        } else {
-        	this.fila=1;
-            throw new IllegalAccessException("ERROR: Fila no valida.");
-        }
-    }
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new IllegalArgumentException("ERROR: No es posible copiar una posicion nula.");
+		}
+		this.fila = posicion.getFila();
+		this.columna = posicion.getColumna();
+	}
 
-    /**
-     * 
-     * @return columa
-     */
-    public char getColumna() {
-        return columna;
-    }
+	/**
+	 * Costructor que recibe dos parametros y controla que los valores introducidos
+	 * son correctos
+	 * 
+	 * @param fila
+	 * @param columna
+	 * @throws IllegalAccessException
+	 */
+	public Posicion(int fila, char columna) {
+		this.fila = fila;
+		this.columna = columna;
+		if (fila > 0 && fila < 9) {
+			this.fila = fila;
+		} else {
+			this.fila = 1;
+			throw new IllegalArgumentException("ERROR: Columna no valida.");
+		}
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			this.columna = 'a';
+			throw new IllegalArgumentException("ERROR: Columna no valida.");
+		}
 
-    /**
-     * Setter que recibe y controla los errores de la columna
-     * @param columna 
-     * @throws IllegalAccessException 
-     */
-    public void setColumna(char columna) throws IllegalAccessException {
-        this.columna = columna;
-        if (columna >= 'a' && columna <= 'h') {
-            this.columna = columna;
-        } else {
-        	this.columna='a';
-            throw new IllegalAccessException("ERROR: Columna no valida.");
-            
-        }
-    }
+	}
+
+	/**
+	 * 
+	 * @return fila
+	 */
+	public int getFila() {
+		return fila;
+	}
+
+	/**
+	 * Setter que recibe y controla los errores de la fila
+	 * 
+	 * @param fila
+	 * @throws IllegalAccessException
+	 */
+	public void setFila(int fila) {
+		this.fila = fila;
+
+		if (fila > 0 && fila < 9) {
+			this.fila = fila;
+		} else {
+			this.fila = 1;
+			throw new IllegalArgumentException("ERROR: Fila no valida.");
+		}
+
+	}
+
+	/**
+	 * 
+	 * @return columa
+	 */
+	public char getColumna() {
+		return columna;
+	}
+
+	/**
+	 * Setter que recibe y controla los errores de la columna
+	 * 
+	 * @param columna
+	 * @throws IllegalAccessException
+	 */
+	public void setColumna(char columna) {
+		this.columna = columna;
+		if (columna >= 'a' && columna <= 'h') {
+			this.columna = columna;
+		} else {
+			this.columna = 'a';
+			throw new IllegalArgumentException("ERROR: Columna no valida.");
+
+		}
+
+	}
+
 }
