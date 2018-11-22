b3a0c08e96e8ff1d105f8125105e2566d409bbec
Bogdan <Bogdan@DESKTOP-1BA6BTE>
2018-11-22 22:41:43 +0100

    Commit despues de crear en constructor con el parametro color

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -4,7 +4,20 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 	
+	public Caballo() {
+		posicion = new Posicion(8, 'b');
+	}
 	
+	public Caballo(Color color) {
+		this.color = color;
+		switch(color) {
+		case BLANCO:
+			posicion = new Posicion(1, 'b');
+			break;
+		case NEGRO:
+			posicion = new Posicion(8, 'b');
+		}
+	}
 	public Color getColor() {
 		return color;
 	}
